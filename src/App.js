4f6c7d13019351bbe5ae183b5a6b7d1e869a719c
hesam <m.hesam.moradian@gmail.com>
2023-09-07 20:13:40 +0330

    header nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import Sidebar from './Components/Sidebar/Sidebar'
+import React from "react";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
+
+import './App.css'
 
 export default function App() {
   return (
-    <Sidebar />
-  )
+    <>
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+        {/* Router */}
+      </div>
+    </>
+  );
 }
