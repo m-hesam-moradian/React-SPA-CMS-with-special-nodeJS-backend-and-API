11d6cbd9e3711d3b5721d425fbc7791bfcf3c5e6
hesam <m.hesam.moradian@gmail.com>
2023-09-08 11:44:23 +0330

    .

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import { MdProductionQuantityLimits } from "react-icons/md";
 import { BiCommentDetail } from "react-icons/bi";
 import { FiUsers } from "react-icons/fi";
 import { BsBagCheck, BsCurrencyDollar } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 import "./Sidebar.css";
 
@@ -14,40 +15,40 @@ export default function Sidebar() {
 
       <ul className="sidebar-links">
         <li>
-          <a href="#">
+          <Link to="/products">
             <AiOutlineHome className="icon" />
             صفحه اصلی
-          </a>
+          </Link>
         </li>
         <li className="active">
-          <a href="#">
+          <Link to="/products">
             <MdProductionQuantityLimits className="icon" />
             محصولات
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
+          <Link to="/comments">
             <BiCommentDetail className="icon" />
             کامنت ها
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
+          <Link to="/users">
             <FiUsers className="icon" />
             کاربران
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
+          <Link to="/orders">
               <BsBagCheck className="icon" />
               سفارشات
-              </a>
+          </Link>
         </li>
         <li>
-          <a href="#">
+          <Link to="/offs">
               <BsCurrencyDollar className="icon" />
               تخفیف ها
-              </a>
+          </Link>
         </li>
       </ul>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./custom.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.querySelector("body"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebV
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
